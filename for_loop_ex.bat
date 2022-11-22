59dbafc33096a8a79f0da1d75cb2c4a3085ebf47
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2022-11-22 13:48:54 +0800

    Create for_loop_ex.bat

---
+++ for_loop_ex.bat
@@ -0,0 +1,8 @@
+rem ref: https://peterju.gitbooks.io/cmddoc/content/loop.html
+rem floder only
+FOR /D %%d IN (*) DO (
+  ECHO %%d
+)
+
+rem find mp4,avi,mpg files
+for %%i in (*.mp4 *.avi *.mpg) DO @echo %%i
