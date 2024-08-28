c8adc54653fd4d2a03f8dbc55879f3771b32d4ae
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2024-08-28 09:50:42 +0800

    Update tesseractdemo.bat

--- tesseractdemo.bat
+++ tesseractdemo.bat
@@ -1,5 +1,7 @@
 rem version
 tesseract --version
+rem  list language
+tesseract --list-langs
 rem using Traditional Chinese  and  write into "output".txt file
 tesseract path_to_your_image.png output -l chi_tra
 rem english  and  write into "output".txt file
